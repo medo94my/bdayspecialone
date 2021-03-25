8f426d73a74ef6c57174a932df19d2db6a6830e7
ahmed tawfik <medoroyalrma@gmail.com>
2021-03-25 20:53:05 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import datetime
+
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    bday=datetime.datetime.now()
+    
+    return render_template('index.html', dt=bday)
+
+
+if __name__ =='__main__':
+    app.run()
+    
