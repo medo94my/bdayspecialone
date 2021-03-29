cfbd2023030dca7d6a9692164a84cb7da9681c31
Dev02 <medoroyalrma@gmail.com>
2021-03-29 21:26:00 +0800

    fixing time

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from flask import Flask,render_template
 import datetime
-
-
 app=Flask(__name__)
-
 @app.route('/')
 def index():
     bday=datetime.datetime.now()
-    
     return render_template('index.html', dt=bday)
 
 
